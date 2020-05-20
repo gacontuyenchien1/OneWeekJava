8e44af82efafecfd124a72ac9dcfe8e3fb2c0bf2
TrongTran <gacontuyenchien1@gmail.com>
2020-05-20 07:25:57 +0700

    Day number 1 with java

---
+++ src/com/oneweekjava/Circle.java
@@ -0,0 +1,46 @@
+/**
+ * @author TrongTran
+ *
+ */
+package com.oneweekjava; 
+
+import java.lang.Math.*;
+
+public class Circle {
+	
+	private double radius;
+	public String color;
+	
+	public Circle(){
+		radius = 1.0;
+		color = "Red";
+	}
+	
+	public Circle(double r){
+		radius = r;
+		color = "Green";
+	}
+	
+	public double getRadius(){
+		return radius;
+	}
+	
+	public String getColor(){
+		return color;
+	}
+	
+	public void setRadius(double r){
+		radius = r;
+	}
+	public void setColor(String color){
+		this.color = color;
+	}
+	
+	public double getArea(){
+		return radius * radius * Math.PI;
+	}
+
+	public String toString(){
+		return "Circle[radius= " + radius + ", color= " + color + "]"; 
+	}
+}
---
+++ src/com/trongtrantest/TestCircle.java
@@ -0,0 +1,37 @@
+package com.trongtrantest;
+
+import com.oneweekjava.Circle;
+
+public class TestCircle {
+	
+	public static final int NCIRCLEARR = 10;
+	
+	public static Circle[] circleArr = new Circle[NCIRCLEARR];
+	
+	
+	public static void main(String[] args) {
+
+		for (int i=0; i<NCIRCLEARR; i++){
+			if ( i % 2 == 0) circleArr[i] = new Circle(i*1.0);
+			else {
+				circleArr[i] = new Circle();
+				circleArr[i].color = "Yellow";
+				
+				// the line below is incorrect, property radius is private 
+				//circleArr[i].radius = 1.0;
+			}
+			
+		}
+		
+		for (int i=0; i<NCIRCLEARR; i++){
+			if ( i % 2 == 0){
+				System.out.println( i+1 +" "+ circleArr[i].toString() 
+						+  "		area: " + Math.round(circleArr[i].getArea()*100)*1.0/100);	
+			}else {	
+				System.out.println( i+1 +" "+ circleArr[i].toString() 
+						+  "		area: " + Math.round(circleArr[i].getArea()*100)*1.0/100);		
+			}
+				
+		}
+	}
+}
